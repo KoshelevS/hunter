8e31e44f40064447b607aafd85821fa202db120a
Sergey Koshelev <xih@yandex.ru>
2016-04-21 17:49:00 +0300

    Create controller and view for list of candidates

---
+++ Hunter6/src/Hunter/Controllers/CandidateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+
+namespace Hunter.Controllers
+{
+    [Route("api/[controller]")]
+    public class CandidateController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
